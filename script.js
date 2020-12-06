01f5202008d07c3eab4f17a35414744f0bea18fa
Farshad Shafii <farshad.shafii@gmail.com>
2020-12-06 22:06:56 +0000

    added color to counter when decrease counter button is clicked

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const number = document.getElementById('number');
-const decrease = document.getElementById('decrease');
-const increase = document.getElementById('increase');
+const number = document.getElementById("number");
+const decrease = document.getElementById("decrease");
+const increase = document.getElementById("increase");
 
-function increaseNum () {
-    increase.addEventListener('click', function() {
-        number.innerHTML++;
-    })
+function increaseNum() {
+  increase.addEventListener("click", function () {
+    number.innerHTML++;
+  });
 }
 
 function decreaseNum() {
-    decrease.addEventListener('click', function() {
-        number.innerHTML--;
-    })
+  decrease.addEventListener("click", function () {
+    number.innerHTML--;
+    if (number.innerHTML < 0) {
+      number.style.color = "red";
+    } else if (number.innerHTML == 0) {
+      number.style.color = "whitesmoke";
+    } else number.style.color = "green";
+  });
 }
 
 increaseNum();
